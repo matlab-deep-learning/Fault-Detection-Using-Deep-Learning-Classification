ee3441798b1f743c203fea81e59f5ed82f38d6b3
David Willingham <dwilling@mathworks.com>
2021-03-31 11:07:19 -0400

    updating for 21a

--- HelperFiles/reloadDatastore.m
+++
@@ -1,9 +0,0 @@
-%   Copyright 2020 The MathWorks, Inc.
-
-% Recreate the datastore again
-
-dataFolder = 'AirCompressorData';
-ads = audioDatastore(dataFolder,'IncludeSubfolders',true,'LabelSource','foldernames');
-rng(3);
-ads = shuffle(ads);
-[adsTrain,adsValidation] = splitEachLabel(ads,0.9,0.1);
